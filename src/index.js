a9a043ff81b97e5c00ddd47a05270eee05ba4318
Markus Murto <markus@murto.eu>
2022-04-08 20:39:25 +0300

    Try to implement authentication

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const layouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const expressSession = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
 const config = require("./config");
 const DB_URI = `mongodb://${config.db.user}:${config.db.pwd}@${config.db.host}:${config.db.port}/${config.db.name}?authMechanism=DEFAULT&authSource=admin`;
 
 const ErrorHandler = require("./controllers/errorhandler");
 const EntityStore = require("./controllers/entitystore");
+const User = require("./models/user");
 
 const app = express();
 
@@ -33,6 +38,28 @@ app.use(
 app.use(express.json());
 app.use(layouts);
 
+app.use(cookieParser);
+app.use(
+  expressSession({
+    secret: "kisse", // TODO: get from env
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app.use(passport.initialize());
+app.use(passport.session());
+
+// create a test user
+try {
+  let testUser = new User({username: "root", email: "foo@bar"});
+  testUser.setPassword("password");
+  testUser.save();
+} catch (error) {}
+
 app.use((req, res, next) => {
   console.log(`A request was made from ${req.url}`);
   next();
@@ -44,9 +71,7 @@ app
   .route("/entitystore")
   .get(EntityStore.renderNewForm)
   .post(EntityStore.saveNewEntityStore);
-app
-  .route("/entitystore/type/:typeStr")
-  .get(EntityStore.renderNewForm);
+app.route("/entitystore/type/:typeStr").get(EntityStore.renderNewForm);
 
 app.get(
   "/:typeStr",
@@ -60,6 +85,30 @@ app
     EntityStore.getSingleEntityStore,
     EntityStore.renderEntityStore
   );
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+  }),
+  (req, res, next) => {
+    req.session.save((error) => {
+      if (error) return next(error);
+      res.redirect("/");
+    });
+  }
+);
+
+app.get("/logout", (req, res, next) => {
+  req.logout();
+  req.session.save((error) => {
+    if (error) return next(error);
+    res.redirect("/login");
+  });
+});
 
 app.use(ErrorHandler.logErrors);
 
