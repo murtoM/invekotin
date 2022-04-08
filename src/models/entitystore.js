65f060667550999e1609a9e48d932729ab0f018c
Markus Ijäs <markus.ijas@outlook.com>
2022-04-08 10:17:33 +0300

    Adds validation for new EntityStore allowedType field

--- src/models/entitystore.js
+++ src/models/entitystore.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
+const config = require("../config");
 
 const schema = new mongoose.Schema({
-  name: String,
-  allowedTypes: [String],
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  allowedTypes: {
+    type: [String],
+    enum: Object.keys(config.entityTypes),
+  },
   entities: [{}]
 });
 
