c23f51f2e0169f84f1b27bd7a6e13b62ac2457c1
Markus Murto <markus@murto.eu>
2022-04-18 20:04:39 +0300

    Add basic login form validation in backend

--- src/strategies/auth/localmongostrategy.js
+++ src/strategies/auth/localmongostrategy.js
@@ -1,10 +1,25 @@
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const User = require("../../models/user");
 
 const localMongoStrategy = new LocalStrategy(
   async (username, password, done) => {
+    try {
+      if (!validator.isAlphanumeric(username, "fi-FI"))
+        throw new Error("Username must contain only alphanumeric characters!");
+
+      if (!validator.isLength(username, { min: 3, max: 128 }))
+        throw new Error("Username length must be between 3 128 characters!");
+
+      if (!validator.isLength(password, { min: 8, max: 71 }))
+        throw new Error("Password length must be between 8 and 71 characters!");
+    } catch (error) {
+      return done(null, false, {
+        message: error.message,
+      });
+    }
     try {
       let user = await User.findOne({ username: username });
 
